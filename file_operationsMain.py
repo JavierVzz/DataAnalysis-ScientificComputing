cf395fefe0873f1590130cb0e1783e9d7f657044
Javier Vazquez <vzz_jav@yahoo.com>
2017-04-26 21:33:36 -0700

    yei!!!!

--- file_operationsMain.py
+++ file_operationsMain.py
@@ -4,9 +4,16 @@
 
 import os, sys
 import file_operationsClass
+import numpy as np
 
 def main():
-     fo = file_operationsClass.file_operations()
+    fo = file_operationsClass.file_operations()
+    fiveTo45 = np.random.random_integers(5, 45, 6)
+    print(fiveTo45)
+    fo.write_my_file(fiveTo45)
+    print(fo.read_my_file())
+
+
 
 
 if __name__ == '__main__':
